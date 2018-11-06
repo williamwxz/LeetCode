7d8ec84f6a0d5e6f682c9bc0420387a7575ce2ef
Zhang, William <williamwxz@gmail.com>
2018-11-06 16:06:18 -0500

    Update DynamicPrgramming/887. Super Egg Drop/Solution.java

--- DynamicPrgramming/887. Super Egg Drop/Solution.java
+++ DynamicPrgramming/887. Super Egg Drop/Solution.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int superEggDrop(int K, int N) {
         int[][] f = new int[N+1][K+1];
         int min=0; //maximum moves for dropping eggs
@@ -6,6 +6,23 @@ class Solution {
             min++;
             for (int k=1; k<=K; k++){
                 f[min][k] = f[min-1][k-1]+f[min-1][k]+1;
+            }
+        }
+        return min;
+    }
+}
+
+class Solution2 {
+    public int superEggDrop(int K, int N) {
+        int[][] f = new int[2][K+1];
+        int min=0; //maximum moves for dropping eggs
+        int cur=0, old;
+        while (f[cur][K]<N){
+            min++;
+            old = cur;
+            cur = 1-cur;
+            for (int k=1; k<=K; k++){
+                f[cur][k] = f[old][k-1]+f[old][k]+1;
             }
         }
         return min;
