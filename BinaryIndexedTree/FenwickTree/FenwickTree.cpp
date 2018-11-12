945c5cac3a72b67a10f63747a290d583ca5c83b0
jmzhang18 <jmzhang18@yahoo.com>
2018-11-12 17:19:35 -0500

    add example of binary index tree

---
+++ BinaryIndexedTree/FenwickTree/FenwickTree.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <stdlib>
+
+using namespace std;
+
+class FenwickTree {
+    public:
+    FenwickTree(int n) : sums_(n+1, 0){
+
+    }
+
+    void update(int i, int delta){
+        while (i<sums_.size()){
+            sums_[i] += delta;
+            i += lowbit(i)
+        }
+    }
+
+    int query(int i){
+        int sum=0;
+        while (i>0){
+            sum += sums_[i];
+            i -= lowbit(i);
+        }
+    }
+
+    private:
+    static inline int lowbit(int x){
+        return x&(-x);
+    }
+    vector<int> sums_;
+}
