c66768d69c897200a88540c5f804e56083b34a1c
William Zhang <williamwxz@gmail.com>
2021-07-29 12:32:02 +0800

    complete karat

--- Robinhood/karat.py
+++ Robinhood/karat.py
@@ -114,113 +114,74 @@ transition_graph(logs2) # =>
 
 '''
 
+
 class SolutionResource:
 
     def user_min_max_time(self, logs: list):
-        res={}
-        MAX=float('inf')
+        res = {}
+        MAX = float('inf')
         for time, user, _ in logs:
             if user not in res:
-                res[user]=[MAX, -MAX]
-            res[user][0]=min(res[user][0], time)
-            res[user][1]=max(res[user][1], time)
+                res[user] = [MAX, -MAX]
+            res[user][0] = min(res[user][0], time)
+            res[user][1] = max(res[user][1], time)
         return res
 
     # most visited resources in 5 mins interval
     # sort logs, resource map, {resource id: [time]}
     # sliding window on each resource, get the max
     def most_requested_resource(self, logs: list):
-        N=len(logs)
+        N = len(logs)
         logs.sort()
-        resourceMap=collections.defaultdict(list)
+        resourceMap = collections.defaultdict(list)
         for time, _, resource in logs:
             resourceMap[resource].append(int(time))
-        
+
         res_resource = ""
         res_count = 0
         FIVE_MIN_IN_SECONDS = 5*60
 
         for resource, times in resourceMap.items():
-            start=0
+            start = 0
             for end in range(len(times)):
                 # inclusve [00:00:00 to 00:05:00]
-                while start<=end and times[start]<times[end]-FIVE_MIN_IN_SECONDS:
-                    start+=1
-                count=end-start+1
-                if count>res_count:
+                while start <= end and times[start] < times[end]-FIVE_MIN_IN_SECONDS:
+                    start += 1
+                count = end-start+1
+                if count > res_count:
                     res_count = count
                     res_resource = resource
         return [resource, res_count]
 
-    
     def transition_graph(self, logs: list):
         # sort logs
         # build graph, {user: [resource]}
         # loop user to resource graph
         # init resource graph["Start"]=[resource at the beginning]
         # resource graph, {resource: [next resource]}
-        N=len(logs)
-        START="START"
-        END="END"
+        N = len(logs)
+        START = "START"
+        END = "END"
         user_graph = collections.defaultdict(list)
         logs.sort()
         for time, user, resource in logs:
             user_graph[user].append(resource)
         # sliding window on user graph
-        resources_map=collections.defaultdict(list)
+        resources_map = collections.defaultdict(list)
         for user, resources in user_graph.items():
             resources_map[START].append(resources[0])
             for i in range(1, len(resources)):
                 resources_map[resources[i-1]].append(resources[i])
             resources_map[resources[-1]].append(END)
-        res={}
+        res = {}
         for start, ends in resources_map.items():
-            count=collections.Counter(ends)
+            count = collections.Counter(ends)
             total = sum(count.values())
             if start not in res:
-                res[start]={}
+                res[start] = {}
             for resource, val in count.items():
-                res[start][resource]=val/total
-        return res
-
-
-
-
-        
-
-if __name__ == "__main__":
-    logs1 = [
-        ["58523", "user_1", "resource_1"],
-        ["62314", "user_2", "resource_2"],
-        ["54001", "user_1", "resource_3"],
-        ["200", "user_6", "resource_5"],
-        ["215", "user_6", "resource_4"],
-        ["54060", "user_2", "resource_3"],
-        ["53760", "user_3", "resource_3"],
-        ["58522", "user_22", "resource_1"],
-        ["53651", "user_5", "resource_3"],
-        ["2", "user_6", "resource_1"],
-        ["100", "user_6", "resource_6"],
-        ["400", "user_7", "resource_2"],
-        ["100", "user_8", "resource_6"],
-        ["54359", "user_1", "resource_3"],
-    ]
-
-    logs2 = [
-        ["300", "user_1", "resource_3"],
-        ["599", "user_1", "resource_3"],
-        ["900", "user_1", "resource_3"],
-        ["1199", "user_1", "resource_3"],
-        ["1200", "user_1", "resource_3"],
-        ["1201", "user_1", "resource_3"],
-        ["1202", "user_1", "resource_3"]
-    ]
-    solution = SolutionResource()
-    print(solution.most_requested_resource(logs1))
-    print(solution.most_requested_resource(logs2))
-
-    print(solution.transition_graph(logs1))
-
+                res[start][resource] = val/total
+        return re
 
 
 '''
@@ -318,3 +279,98 @@ n: number of pairs in the input
 
 */
 '''
+
+
+class Solution2:
+
+    # build graph
+    # dfs
+    # when at the end, like no children for this course, update result
+    # start with the courses 0 indgree
+    def get_middle_class(self, courses: list):
+        N = len(courses)
+        indegrees = collections.defaultdict(int)
+        graph = collections.defaultdict(list)
+        for pre, cur in courses:
+            indegrees[cur] += 1
+            if pre not in indegrees:
+                indegrees[pre] = 0
+            graph[pre].append(cur)
+        middle = []
+
+        def dfs(course: str, builder=[]):
+            if len(graph[course]) == 0:
+                M = len(builder)
+                middle.append(builder[(M-1)//2])
+                return
+            for child in graph[course]:
+                builder.append(child)
+                dfs(child, builder)
+                builder.pop(-1)
+            return
+
+        for course, count in indegrees.items():
+            if count == 0:
+                dfs(course, builder=[course])
+        return list(set(middle))
+
+
+if __name__ == "__main__":
+    # logs1 = [
+    #     ["58523", "user_1", "resource_1"],
+    #     ["62314", "user_2", "resource_2"],
+    #     ["54001", "user_1", "resource_3"],
+    #     ["200", "user_6", "resource_5"],
+    #     ["215", "user_6", "resource_4"],
+    #     ["54060", "user_2", "resource_3"],
+    #     ["53760", "user_3", "resource_3"],
+    #     ["58522", "user_22", "resource_1"],
+    #     ["53651", "user_5", "resource_3"],
+    #     ["2", "user_6", "resource_1"],
+    #     ["100", "user_6", "resource_6"],
+    #     ["400", "user_7", "resource_2"],
+    #     ["100", "user_8", "resource_6"],
+    #     ["54359", "user_1", "resource_3"],
+    # ]
+
+    # logs2 = [
+    #     ["300", "user_1", "resource_3"],
+    #     ["599", "user_1", "resource_3"],
+    #     ["900", "user_1", "resource_3"],
+    #     ["1199", "user_1", "resource_3"],
+    #     ["1200", "user_1", "resource_3"],
+    #     ["1201", "user_1", "resource_3"],
+    #     ["1202", "user_1", "resource_3"]
+    # ]
+    # solution = SolutionResource()
+    # print(solution.most_requested_resource(logs1))
+    # print(solution.most_requested_resource(logs2))
+
+    # print(solution.transition_graph(logs1))
+
+    all_courses_1 = [["Logic", "COBOL"],
+                     ["Data Structures", "Algorithms"],
+                     ["Creative Writing", "Data Structures"],
+                     ["Algorithms", "COBOL"],
+                     ["Intro to Computer Science", "Data Structures"],
+                     ["Logic", "Compilers"],
+                     ["Data Structures", "Logic"],
+                     ["Graphics", "Networking"],
+                     ["Networking", "Algorithms"],
+                     ["Creative Writing", "System Administration"],
+                     ["Databases", "System Administration"],
+                     ["Creative Writing", "Databases"],
+                     ["Intro to Computer Science", "Graphics"],
+                     ]
+
+    all_courses_2 = [["Course_3", "Course_7"],
+                     ["Course_0", "Course_1"],
+                     ["Course_1", "Course_2"],
+                     ["Course_2", "Course_3"],
+                     ["Course_3", "Course_4"],
+                     ["Course_4", "Course_5"],
+                     ["Course_5", "Course_6"],
+                     ]
+    solution = Solution2()
+    print(solution.get_middle_class(all_courses_1))
+    print(solution.get_middle_class(all_courses_2))
